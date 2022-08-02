2c4406557fbeac3b7465e22d2a9f3a8266fad9c8
Yash Arora <yasharora102@gmail.com>
2022-08-02 17:32:04 +0530

    Update omdb.py

--- omdb.py
+++ omdb.py
@@ -1,44 +1,69 @@
 import requests
-import argparse
+# import argparse
 import json
 import csv
 
-parser = argparse.ArgumentParser(description="Movie Watchlist")
-parser.add_argument("--l", required=True, type=int,
-                    help="No. of movies to add in list")
-parser.add_argument("--api_key", default='YOUR_KEY_GOES_HERE',
-                    type=str, help="API Key")
+# we can add these filters but I thought they will increase the workload as they don't have a purpose here.
 
-args = parser.parse_args()
-yourkey = args.api_key
-list_length = args.l
-csvheader = ['Title', "Year", 'Released', "imdbRating"]
-moviedata = []
+# parser = argparse.ArgumentParser(description="Movie Watchlist")
+# parser.add_argument("--l", required=True, type=int,
+#                     help="No. of movies to add in list")
+# parser.add_argument("--api_key", default='',
+#                     type=str, help="API Key")
+# args = parser.parse_args()
+# yourkey = args.api_key
+# list_length = args.l
 
-with open("movie.csv", 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    writer.writerow(csvheader)
-    images_saved = 0
-    for i in range(list_length):
+yourkey = 'Your_Key_Goes_Here'
+def create_dataset(lst):
+    csvheader = ['Title', "Year", 'Released', "imdbRating"]
+    moviedata = []
 
-        movie_name = input("Enter movie name\t")
-        api_url = f"http://www.omdbapi.com/?apikey={yourkey}&t={movie_name}"
-        request = requests.get(api_url)
-        parsed_request = json.loads(request.text)
+    with open("dataset.csv", 'w', encoding='UTF8') as f:
+        print("Creating dataset...")
+        print("Total Items: ", len(lst))
+        
+        writer = csv.writer(f)
+        writer.writerow(csvheader)
+        images_saved = 0
+        count = 0       
+        for movie_name in lst:
+            
+            api_url = f"http://www.omdbapi.com/?apikey={yourkey}&t={movie_name}"
+            request = requests.get(api_url)
+            parsed_request = json.loads(request.text)
 
-        if 'Error' in parsed_request:
-            print(parsed_request['Error'])
+            if 'Error' in parsed_request:
+                print(parsed_request['Error'])
 
-        else:
-            listing = [parsed_request["Title"], parsed_request["Year"],
-                       parsed_request["Released"], parsed_request["imdbRating"]]
-            moviedata.append(listing)
-            movie_title = parsed_request["Title"]
-            photo_url = parsed_request["Poster"]
-            photo_request = requests.get(photo_url)
-            photo_path = open(f"{movie_title}.png", "wb")
-            photo_path.write(photo_request.content)
-            photo_path.close()
+            else:
+                # code for making a list of movies
+                listing = [parsed_request["Title"], parsed_request["Year"],
+                           parsed_request["Released"], parsed_request["imdbRating"]]
+                moviedata.append(listing)
+                print("Item: ", count+1)
+                count += 1
+            #since we are not saving the images, we can comment this out. 
+            # code for saving the images
 
-    writer.writerows(moviedata)
-    f.close()
+                # movie_title = parsed_request["Title"]
+                # photo_url = parsed_request["Poster"]
+                # photo_request = requests.get(photo_url)
+                # photo_path = open(f"{movie_title}.png", "wb")
+                # photo_path.write(photo_request.content)
+                # photo_path.close()
+        print("Dataset created successfully!")        
+        writer.writerows(moviedata)
+        f.close()
+
+# function for extracting the data from the txt file
+def get_data():
+    lst = []
+    with open("data.txt", 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            lst.append(line)
+    return lst
+
+create_dataset(get_data())
