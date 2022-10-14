424d147d4ac94ebce6d57e0edabaaeda8a6366e0
Yash Arora <yasharora102@gmail.com>
2022-10-14 18:45:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,36 +3,39 @@ import telebot
 import requests
 import json
 import csv
+import uuid
 
 yourkey = os.getenv("yourkey")
 bot_id = os.getenv("bot_id")
 
 bot = telebot.TeleBot(bot_id)
 
-
 @bot.message_handler(commands=['start', 'hello'])
 def greet(message):
-    global botRunning
-    botRunning = True
-    global file_created
-    file_created = False
-
-    bot.reply_to(
-        message, 'Hello there! I am a bot that will show movie information for you and export it in a CSV file.\n\n')
+  global botRunning
+  global unique_filename
+  unique_filename = 'movie' + str(uuid.uuid1()) + ".csv"
+  botRunning = True
+  bot.reply_to(
+    message,
+    'Hello there! I am a bot that will show movie information for you and export it in a CSV file.\n\n'
+  )
 
 
 @bot.message_handler(commands=['stop', 'bye'])
 def goodbye(message):
-    global botRunning
-    botRunning = False
-    bot.reply_to(message, 'Bye!\nHave a good time')
-    os.remove('movie.csv')
-    moviedata.clear()
+  global botRunning
+  botRunning = False
+  bot.reply_to(message, 'Bye!\nHave a good time')
+  moviedata.clear()
 
 
 @bot.message_handler(func=lambda message: botRunning, commands=['help'])
 def helpProvider(message):
-    bot.reply_to(message, '1.0 You can use \"/movie MOVIE_NAME\" command to get the details of a particular movie. For eg: \"/movie The Shawshank Redemption\"\n\n2.0. You can use \"/export\" command to export all the movie data in CSV format.\n\n3.0. You can use \"/stop\" or the command \"/bye\" to stop the bot.')
+  bot.reply_to(
+    message,
+    '1.0 You can use \"/movie MOVIE_NAME\" command to get the details of a particular movie. For eg: \"/movie The Shawshank Redemption\"\n\n2.0. You can use \"/export\" command to export all the movie data in CSV format.\n\n3.0. You can use \"/stop\" or the command \"/bye\" to stop the bot.'
+  )
 
 
 moviedata = []
@@ -40,70 +43,71 @@ moviedata = []
 
 @bot.message_handler(func=lambda message: botRunning, commands=['movie'])
 def getMovie(message):
-    bot.reply_to(message, 'Getting movie info...')
-    text_data = message.text
-    movie_name_data = text_data.replace('/movie ', '')
-    print(movie_name_data)
+  bot.reply_to(message, 'Getting movie info...')
+  # TODO: 1.2 Get movie information from the API
+  text = message.text
+  text2 = text.replace('/movie ', '')
+  print(text2)
 
-    lst = []
-    if ',' in movie_name_data:
-        lst = movie_name_data.split(',')
-    else:
-        lst.append(movie_name_data)
-    print(lst)
+  lst = []
+  if ',' in text2:
+    lst = text2.split(',')
+  else:
+    lst.append(text2)
+  print(lst)
 
-    with open("movie.csv", 'w', encoding='UTF8') as f:
-        file_created = True
-        writer = csv.writer(f)
-        csvheader = ['Title', "Year", 'Released', "imdbRating"]
-        writer.writerow(csvheader)
+  csvheader = ['Title', "Year", 'Released', "imdbRating"]
+  with open('movie_data/' + unique_filename, 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(csvheader)
+    images_saved = 0
 
-        for i in lst:
-            movie_name = str(i)
-            api_url = f"http://www.omdbapi.com/?apikey={yourkey}&t={movie_name}"
-            request = requests.get(api_url)
-            parsed_request = json.loads(request.text)
+    for i in lst:
+      movie_name = str(i)
+      api_url = f"http://www.omdbapi.com/?apikey={yourkey}&t={movie_name}"
+      request = requests.get(api_url)
+      parsed_request = json.loads(request.text)
 
-            if 'Error' in parsed_request:
-                print(parsed_request['Error'])
-                reply = 'Movie not found!, Please try again'
-                bot.send_message(message.chat.id, reply)
+      if 'Error' in parsed_request:
+        print(parsed_request['Error'])
+        reply = 'Movie not found!, Please try again'
+        bot.send_message(message.chat.id, reply)
 
-            else:
-                # code for movies
-                listing = [parsed_request["Title"], parsed_request["Year"],
-                           parsed_request["Released"], parsed_request["imdbRating"]]
-                moviedata.append(listing)
+      else:
+        # code for making a list of movies
+        listing = [
+          parsed_request["Title"], parsed_request["Year"],
+          parsed_request["Released"], parsed_request["imdbRating"]
+        ]
+        moviedata.append(listing)
 
-                # code for the images
-                movie_title = parsed_request["Title"]
-                photo_url = parsed_request["Poster"]
-                photo_request = requests.get(photo_url)
+        # code for saving the images
+        movie_title = parsed_request["Title"]
+        photo_url = parsed_request["Poster"]
+        photo_request = requests.get(photo_url)
 
-                reply = "Movie found!"
-                caption = "Movie Name: " + parsed_request["Title"] + "\n" + "Year: " + parsed_request["Year"] + "\n" + \
-                    "Released: " + \
-                    parsed_request["Released"] + "\n" + \
-                    "imdbRating: " + parsed_request["imdbRating"]
-                bot.send_message(message.chat.id, reply)
-                bot.send_photo(message.chat.id, photo_url, caption=caption)
+        reply = "Movie found!"
+        caption = "Movie Name: " + parsed_request[
+          "Title"] + "\n" + "Year: " + parsed_request[
+            "Year"] + "\n" + "Released: " + parsed_request[
+              "Released"] + "\n" + "imdbRating: " + parsed_request["imdbRating"]
+        bot.send_message(message.chat.id, reply)
+        bot.send_photo(message.chat.id, photo_url, caption=caption)
 
-        writer.writerows(moviedata)
+    writer.writerows(moviedata)
 
 
 @bot.message_handler(func=lambda message: botRunning, commands=['export'])
-def getList(message):
-    if file_created:
-        bot.reply_to(message, 'Generating file...')
-        bot.reply_to(message, 'File generated!')
-        bot.send_document(message.chat.id, open('movie.csv', 'rb'))
-    else:
-        bot.reply_to(message, 'No file to export!')
+def export_file(message):
+  bot.reply_to(message, 'Generating file...')
+  bot.reply_to(message, 'File generated!')
+  doc = open('movie_data/' + unique_filename, 'rb')
+  bot.send_document(message.chat.id, doc)
 
 
 @bot.message_handler(func=lambda message: botRunning)
 def default(message):
-    bot.reply_to(message, 'I did not understand '+'\N{confused face}')
+  bot.reply_to(message, 'I did not understand ' + '\N{confused face}')
 
 
 bot.infinity_polling()
